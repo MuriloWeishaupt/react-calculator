21865ab9af750de89b2be0ef21140ba33c196ab5
MWeishaupt <113941985+MuriloWeishaupt@users.noreply.github.com>
2025-08-25 23:29:32 +0000

    Adicionada lógica inicial da calculadora

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,7 +1,74 @@
 import Buttons from './components/Buttons'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const initialStates = [{
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+  }]
+
+  const [states, setStates] = useState(...initialStates);
+
+  function addNumber(number) {
+    if (number === '.' && states.displayValue.includes(".")) {
+      return
+    }
+
+    const clearDisplay = clearDisplay ? '' : states.displayValue;
+
+    const displayValue = currentValue + number;
+
+    setStates(copyValue => {
+      return {...copyValue, displayValue, clearDisplay: false}
+    });
+
+    const newValue = parseFloat(displayValue);
+
+    const values = [...states.value];
+    values[states.current] = newValue;
+    setStates(copyValue => {
+      return {...copyValue, values}
+    });
+  }
+
+  function clearMemory() {
+    setStates(...initialStates);
+  }
+
+  function setOperation(operation) {
+    if(states.current === 0) {
+      setStates(copyValue => {
+        return {...copyValue, operation, current: 1, clearDisplay: true}
+      });
+    } else {
+      const equals = operation === '=';
+      const values = [...states.values];
+
+      try {
+        values[0] = eval(`${values[0]} ${states.operation} ${values[1]}`);
+      } catch (error) {
+        values[0] = states.values[0];
+      }
+
+      values[1] = 0;
+
+      setStates(copyValue => {
+        return {
+          ...copyValue,
+          displayValue: values[0],
+          operation: equals ? null : operation,
+          current: equals ? 0: 1,
+          clearDisplay: true,
+          values,
+        }
+      });
+    }
+  }
 
   return (
     <>
